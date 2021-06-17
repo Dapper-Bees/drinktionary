c5ce06ba18d87ac63bc9db7b93dedfec33490295
AJ Tran <31839316+ajtran303@users.noreply.github.com>
2021-06-16 23:23:46 -0400

    Add Drinks model test
    It has validation tests

---
+++ spec/models/drink_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe Drinks, type: :model do
+
+  describe 'validations' do
+    it { should validate_presence_of :id }
+    it { should validate_numericality_of(:id).only_integer }
+    it { should validate_presence_of :name }
+    it { should validate_presence_of :cost }
+    it { should validate_numericality_of(:cost).only_integer }
+  end
+
+end
