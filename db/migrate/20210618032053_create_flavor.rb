b07609b4a91055c913412eb98ead79e98b213b33
AJ Tran <31839316+ajtran303@users.noreply.github.com>
2021-06-17 23:25:25 -0400

    Migrate flavors table

---
+++ db/migrate/20210618032053_create_flavor.rb
@@ -0,0 +1,7 @@
+class CreateFlavor < ActiveRecord::Migration[6.1]
+  def change
+    create_table :flavors do |t|
+      t.string :name
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_18_031410) do
+ActiveRecord::Schema.define(version: 2021_06_18_032053) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,10 @@ ActiveRecord::Schema.define(version: 2021_06_18_031410) do
   create_table "drinks", force: :cascade do |t|
     t.string "name"
     t.integer "cost"
+  end
+
+  create_table "flavors", force: :cascade do |t|
+    t.string "name"
   end
 
 end
